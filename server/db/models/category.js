da811c293a494baa989abc07b55060157158e605
the97bradley <38818584+the97bradley@users.noreply.github.com>
2018-07-23 15:28:47 -0500

    Update category.js

--- server/db/models/category.js
+++ server/db/models/category.js
@@ -3,7 +3,10 @@ const db = require('../db')
 
 const Category = db.define('category', {
 	name: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+    allowNull: false,
+    validate : {
+      notEmpty : true
 	}
 })
 
