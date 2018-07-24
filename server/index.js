f22ecde5d1a38496ff14c8ca30eef46b647f737a
Kevin Lim <38468417+kevslim@users.noreply.github.com>
2018-07-24 11:26:26 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -65,7 +65,6 @@ const createApp = () => {
   // auth and api routes
   app.use('/auth', require('./auth'))
   app.use('/api', require('./api'))
-  console.log("!!!@@#$!#$!@#$!#$!@#$!@#$!@#$!#@$!@#$!@#$!@#$!@#$!@#$!#@")
   
   // static file-serving middleware
   app.use(express.static(path.join(__dirname, '..', 'public')))
