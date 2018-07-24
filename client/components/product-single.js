67d32e772d5c5cb705fef73e066d1d326674f360
Kevin Lim <38468417+kevslim@users.noreply.github.com>
2018-07-24 14:17:57 -0500

    starting single product

--- client/components/product-single.js
+++ client/components/product-single.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
-
 class ProductSingle extends React.Component {
-    constructor(props){
-        super(props)
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: 1
     }
+  }
 
-    render(){
-        
-    }
+  componentDidMount() {
 
+  }
+
+  render() {
+    return <h1>Hello</h1>
+  }
 }
+
+/**
+ * CONTAINER
+ */
+const mapState = state => {
+  return {
+    product: state.product
+  }
+}
+
+const mapDispatch = (state, id) => {
+    return {
+      getProduct: state.
+    }
+  }
+
+export default connect(mapState)(ProductSingle)
