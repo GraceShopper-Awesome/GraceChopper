d279e5e922c6033422662b1dfbfd2178dde54b7f
zfarooqi <farooqi1992@gmail.com>
2018-07-25 18:00:24 -0500

    got a dumb shopping cart view

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,29 +1,66 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import { Link } from 'react-router-dom'
+import { products } from '../store'
 
 
 class Cart extends React.Component {
     componentDidMount() {
-        console.log("HIIIIIIIIIIII")
+        this.props.getProduct()
+        this.handleChange= this.handleChange.bind(this)
+        this.handleRemove= this.handleRemove.bind(this)
     }
 
+    handleChange(){
 
+    }
+
+    handleRemove(){
+
+    }
 
     render(){
         console.log("This.props.cart", this.props.cart)
         return(
         <div>
-            <p>hello</p>
+            {/* {this.props.cart.map(element => (
+                <h1 key={element.id}>{element.id}</h1>
+            ))} */}
+            <div id="itemList">
+            <div id="singleItem">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/LAPD_Bell_206_Jetranger.jpg"/>
+            </div>
+            <div id="itemText">
+                <h2>Product Name</h2>
+                <h2>$Price.00</h2>
+                <form>
+                {/* max is quantity, default value is 1 or previous quantity*/}
+                <label htmlFor="quantity">Quantity</label>
+                    <input type="number" name="quantity" min="1"  onChange={(evt) => handleChange(evt)}/>
+                </form>
+            </div>
+            <p>Lorem Ipsum suck a dick.</p>
+        </div>
+            <button id="checkoutButton">Proceed to Checkout</button>
         </div>
         )
     }
 }
 
+{/* <label htmlFor='campusId'><h4>Campus</h4></label>
+<select name='campusId' onChange={this.handleChange}>
+    {this.props.campuses.map((campus) => (<option name='campusId' value = {campus.id} key = {campus.id}>{campus.name}</option>))}
+</select> */}
+
 const mapStateToProps = state => {
     return {
-      cart: state.cart
+      cart: state.cart,
+      product: state.products.products
     }
   }
 
-export default connect(mapStateToProps)(Cart)
+const mapDispatchToProps = dispatch => {
+    return  {getProduct : () => dispatch(products())}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
--- client/components/product-single.js
+++ client/components/product-single.js
@@ -2,17 +2,15 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {singleProduct, addToCart} from '../store'
 
-
 class ProductSingle extends React.Component {
   constructor(props) {
     super(props)
-
 
     this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
-    const {id} = this.props.match.params;
+    const {id} = this.props.match.params
     this.props.getProduct(id)
   }
 
@@ -25,15 +23,15 @@ class ProductSingle extends React.Component {
     const {title, description, price, imageUrl, stock} = this.props.product
     return (
       <div>
-        <div id="productSingle"> 
-        <h1>Product Name: {title}</h1>
-        <p>Description: {description}</p>
-        <h2>Price: {price}</h2>
-        <h3>Stock: {stock}</h3>
-        {imageUrl && imageUrl.length && imageUrl.map(el => <img src={el} />)}
-      </div>
-        <button onClick={(evt) => this.handleClick(evt)}>ADD TO CART!</button>
+        <div id="productSingle">
+          <h1>Product Name: {title}</h1>
+          <p>Description: {description}</p>
+          <h2>Price: {price}</h2>
+          <h3>Stock: {stock}</h3>
+          {imageUrl && imageUrl.length && imageUrl.map(el => <img src={el} />)}
         </div>
+        <button onClick={evt => this.handleClick(evt)}>ADD TO CART!</button>
+      </div>
     )
   }
 }
@@ -48,11 +46,10 @@ const mapState = state => {
 }
 
 const mapDispatch = dispatch => {
-    return {
-      getProduct: id => dispatch(singleProduct(id)),
-      addAProduct: product => dispatch(addToCart(product))
-    }
+  return {
+    getProduct: id => dispatch(singleProduct(id)),
+    addAProduct: product => dispatch(addToCart(product))
   }
-
+}
 
 export default connect(mapState, mapDispatch)(ProductSingle)
