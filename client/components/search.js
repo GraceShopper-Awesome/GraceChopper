50425275a305b50b58e0f55ac1a94bb0f254acaa
Brad Smith <bradsmith712@gmail.com>
2018-07-31 22:23:16 -0500

    misc styling

--- client/components/search.js
+++ client/components/search.js
@@ -2,20 +2,20 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
 export default class Search extends Component {
-	constructor() {
-		super()
-		this.state = {
-			value: ""
-		}
-	}
+  constructor() {
+    super()
+    this.state = {
+      value: ''
+    }
+  }
 
-	render() {
-		return (
-			<div>
-				<label>Search</label>
-				<input type="text" name="search"/>
-				<button type="submit">Search</button>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <label>Search</label>
+        <input type="text" name="search" />
+        <button type="submit">Search</button>
+      </div>
+    )
+  }
 }
